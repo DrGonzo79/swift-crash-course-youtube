2e3d1972f5714a7fdfb18e88c6d5c72397df34ad
Justin T <98497366+DrGonzo79@users.noreply.github.com>
2023-12-06 14:26:24 -0600

    inital commit

--- operators.playground/Contents.swift
+++ operators.playground/Contents.swift
@@ -1,4 +1,16 @@
-import UIKit
+import Foundation
 
-var greeting = "Hello, playground"
+let myAge = 22
+let yourAge = 20
+
+if myAge > yourAge {
+    "I'm older than you"
+} else if myAge < yourAge {
+    "I'm younger than you"
+} else {
+    "Oh hey, we are the same age"
+}
+
+let myMothersAge = myAge + 30
+let doubleMyAge = myAge * 2
 

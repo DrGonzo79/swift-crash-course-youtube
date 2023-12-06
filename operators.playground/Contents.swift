8d11da1f0bcc8e6a7a638117026cdbbfe76c1fc5
Justin T <98497366+DrGonzo79@users.noreply.github.com>
2023-12-06 14:41:44 -0600

    operators added

--- operators.playground/Contents.swift
+++ operators.playground/Contents.swift
@@ -11,6 +11,32 @@ if myAge > yourAge {
     "Oh hey, we are the same age"
 }
 
-let myMothersAge = myAge + 30
-let doubleMyAge = myAge * 2
+let myMothersAge = myAge + 30 // binary infix operator (sitting between)
+let doubleMyAge = myAge * 2 //
+
+/// 1. unary prefix
+let foo = !true
+/// 2. unary postfix (very rare to create yourself)
+let name = Optional("Vandad")
+let unaryPostFix = name!
+type(of: unaryPostFix)
+/// 3. binary infix
+let result = 1 + 2
+let names = "Foo" + " " + "Bar"
+
+
+/// 4. Ternary operator
+let age = 30
+//let message: String
+//if age >= 18 {
+//    message = "you are an adult"
+//} else {
+//    message = "you are not an adult"
+//}
+
+let newAge = age - 15
+let message = newAge >= 18
+    ? "you are an adult"
+    : "You are not an adult"
+
 
